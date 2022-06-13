09210db554622a673c4d27eb0910aa193db35cea
ARJUN <arjunsharman06@gmail.com>
2022-06-13 01:52:58 -0400

    added the current day script

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,34 +42,92 @@ var longLatitude =  (cityName) => {
   let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${maxCityLimit}&appid=${key}`;
   apiFunction(url)
   .then(data => {
-      if (data.length > 0) {
-        currentWeather(data[0].lon, data[0].lat);
+      if (data != null) {
+        totalWeather(data[0].lon, data[0].lat,cityName);
       } else {
         return false;
       }
   });
 };
 
-// API call to calculate current weather
-var currentWeather = async function (longitude, latitude) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+// API call to get  current , 5 days  weather details
+var totalWeather =  (longitude, latitude,cityName) => {
+  // let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&units=imperial&appid=${key}`;
   apiFunction(url)
   .then(data => {
-      if (data.length > 0) {
+    console.log(data);
+      if (data != null) {
+        // call current Weather
+        currentWeather(data["current"],cityName);
 
+        // call 5 days forecast
+        forcastWeather(data["daily"]);
       }
   });
 }
 
+// Current Weather
+var currentWeather = (weatherObject , name)=>{
+debugger;
+  
+  let cityName = document.createElement("span");
+  const currentDate = new Date(weatherObject.dt*1000);
+  let icon = weatherObject.weather[0].icon;
+
+  // Adding the city name
+  cityName.classList = "display-5",
+  cityName.innerHTML = `<h3>${name}&nbsp(${currentDate.toLocaleDateString("en-US")})<img src="http://openweathermap.org/img/wn/${icon}@2x.png" height="50vh"></img></h3`;
+
+  currentDayEl.innerHTML="";
+  currentDayEl.appendChild(cityName);
+
+  // Current Temp
+  let currentTemp = document.createElement("h5");
+  currentTemp.innerHTML = `Temp&nbsp:&nbsp${weatherObject.temp}&#176;F`;
+
+  // Current Wind Speed
+  let currentWindSpeed = document.createElement("h5");
+  currentWindSpeed.innerHTML = `Wind&nbsp:&nbsp${weatherObject.wind_speed}MPH`;
+ 
+
+  // Current Humidity
+  let currentHumidity = document.createElement("h5");
+  currentHumidity.innerHTML = `Humidity&nbsp:&nbsp${weatherObject.humidity}%`;
+ 
+   // UV Index
+   let currentUVI = document.createElement("h5");
+   let uv = Math.round(parseFloat (weatherObject.uvi));
+
+  //  Color of UV according to severity
+   if(uv>=0 && uv<=2){
+    color="green"
+   }else if(uv>=3 && uv<=5){
+    color="yellow";
+   }else if(uv>=6 && uv<=7){
+    color="orange";
+   }else{
+    color="red";
+   } 
+
+   currentUVI.innerHTML = `UV Index &nbsp:&nbsp<label class="uv" style="background-color:${color}">${weatherObject.uvi}</label>`;
+
+  //  Appending to the parent
+   currentDayEl.appendChild(currentTemp);
+   currentDayEl.appendChild(currentWindSpeed);
+   currentDayEl.appendChild(currentHumidity);
+   currentDayEl.appendChild(currentUVI);
+}
+
 // API calls
-const apiFunction =  async function (url) {
+const apiFunction =  async  (url) => {
    let response = await fetch(`${url}`);
    let data = await response.json();
    return data;
 };
 
 // Capitilising the first letter of each word
-var titleCase = function(str) {
+var titleCase = (str) => {
   var tStr = "";
  str.split(" ").forEach(element => {
   tStr = tStr + (element.charAt(0).toUpperCase() + element.slice(1) + " ");
